c71778d699c2807e1d63bd4cc00069df282e6440
Grzegorz Rakowski <grzegorz.m.rakowski@gmail.com>
2022-04-25 20:27:31 +0200

    Create view: update 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://taskskodilla.herokuapp.com/v1/tasks';
-  const trelloApiRoot = 'https://taskskodilla.herokuapp.com/1';
+  const trelloApiRoot = 'https://taskskodilla.herokuapp.com/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
